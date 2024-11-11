dba7e1cbecd1ea07228ab732c5b6684ea89e6da9
snp06 <131759410+snp06@users.noreply.github.com>
2024-11-11 11:37:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector('.container')
 const unsplashURL = 'https://unsplash.com/s/photos/random'
 const rows = 5
 
+for(let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img')
+    img.src = `${unsplashURL}${getRandomSize()}`
+    container.appendChild(img)
+}
 
 function getRandomSize() {
     return `${getRandomNr()}x${getRandomNr()}`
